fabe46a99e6330c80495d43b6548c9aaf979ff7e
RockChanel <rockchanel@icloud.com>
2018-06-28 14:04:48 +0800

    添加注释

--- SWForm/UIImageView+FormImage.h
+++ SWForm/UIImageView+FormImage.h
@@ -9,14 +9,14 @@
 #import <UIKit/UIKit.h>
 
 /**
- 图片附件加载，去除框架对SDWebImage引用
+ 图片附件加载
  */
 @interface UIImageView (FormImage)
 
 
 /**
  图片附件条目图片加载
-
+ 可以在此使用 SDWebImage
  @param url 图片加载的url
  */
 - (void)sw_setImageItemWithUrl:(NSURL *)url;
